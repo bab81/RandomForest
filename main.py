7ac5d7dd67a6ba625b8615ee95b88492840213f1
Bilal Asghar <bb@skbglobal.com>
2021-06-22 14:51:37 -0400

    initial

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+from pylab import *
+from numpy.matlib import repmat
+import sys
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+import time
+from helper import *
+
+print('You\'re running python %s' % sys.version.split(' ')[0])
